d9efb2cf76f7e216fa08133c7b7f0a0cf1a4b76d
jianwu <wujian...199462>
2019-01-21 16:47:49 +0800

    jerry

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,29 +1,17 @@
 package com.example.demo;
 
-import com.example.demo.domain.User;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.sf.json.JSONObject;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClients;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @ServletComponentScan //目前用于实时监听session的
 
 @EnableCaching
 @EnableScheduling//启动定时任务配置
-public class DemoApplication {
+public class  DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
